1199d01a15d7634cd6266dc491a74e37dbb8452f
Visa Haarala <visa.haarala@terramonitor.com>
2022-08-13 18:54:39 +0300

    doodling with algorithm for the maze

--- react/src/App.js
+++ react/src/App.js
@@ -1,10 +1,71 @@
-function App() {
+import Maze from './tools/Maze';
 
-  
+function App() {
+  // create Maze
+  const maze = Maze([40, 50]);
+
+  // Create two dimensional array filled with walls
+  //
+  // x range: 0 ... (maze.x_max * 2 - 1)
+  // y range: 0 ... (maze.y_max * 2 - 1)
+  //
+  // true === wall
+  // false === no wall
+
+  const wallsArray = [];
+
+  for (let i = 0; i < maze.x_max * 2 + 1; i++) {
+    const row = [];
+    for (let j = 0; j < maze.y_max * 2 + 1; j++) {
+      row.push(true);
+    }
+    wallsArray.push(row);
+  }
+
+  // make routes (false, no wall) to the array
+  // starts with an X at "[0,0]"" which is actually [1,1] in the walls array because [0,0] is the corner wall
+  wallsArray[1][1] = false;
+
+  maze.routesArray.forEach((route) => {
+    // console.log(route);
+    const x1 = route[0][0] * 2 + 1;
+    const x2 = route[1][0] * 2 + 1;
+    const y1 = route[0][1] * 2 + 1;
+    const y2 = route[1][1] * 2 + 1;
+
+    // visualArray[x1][y1] = 'X';
+    wallsArray[(x1 + x2) / 2][(y1 + y2) / 2] = false;
+    wallsArray[x2][y2] = false;
+  });
+
+  // create exit & entry points
+
+  // randomly: one on the top and one on the bottom
+  // wallsArray[0][Math.floor(Math.random() * (maze.y_max)) * 2 + 1] = false;
+  // wallsArray[maze.x_max * 2][Math.floor(Math.random() * (maze.y_max)) * 2 + 1] = false;
+
+  // or at opposite corners, top & bottom
+  wallsArray[0][1] = false;
+  wallsArray[maze.x_max * 2][maze.y_max * 2 - 1] = false;
 
   return (
     <main>
       <h1>labyrintti</h1>
+      <div className='maze'>
+        {wallsArray.map((row) => {
+          return (
+            <div>
+              {row.map((point) => {
+                if (point) {
+                  return <b>&#9632;</b>;
+                } else {
+                  return <b>&nbsp;</b>;
+                }
+              })}
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
--- react/src/index.js
+++ react/src/index.js
@@ -1,5 +1,14 @@
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  // <StrictMode>
+    <App />
+  // </StrictMode>
+);
